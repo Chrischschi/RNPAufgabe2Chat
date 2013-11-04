cb48cf3d18625b223f381a49c5b24419787d5178
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-11-05 00:35:20 +0100

    Testklasse für ChatServer erstellt

---
+++ test/server_test/ChatServerTest.java
@@ -0,0 +1,19 @@
+package server_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChatServerTest {
+
+	@Test
+	public void testLogClientIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddClient() {
+		fail("Not yet implemented");
+	}
+
+}
