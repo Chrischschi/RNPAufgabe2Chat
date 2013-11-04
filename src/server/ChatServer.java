8d24f480b388a54fd719046596124d2023836261
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-11-04 22:09:17 +0100

    Erster Commit für RNP 2, klassen angelegt

---
+++ src/server/ChatServer.java
@@ -0,0 +1,18 @@
+package server;
+
+public class ChatServer {
+	//Der Server wartet auf TCP-Verbindungsanfragen auf Port 50000
+	public static final int PORT_NUMBER = 50000; 
+	
+	
+	
+	//und startet einen neuen Arbeits-Thread für jede Verbindung,
+	class ChatServerWorkThread extends java.lang.Thread {
+		
+	}
+	
+
+}
+
+
+
