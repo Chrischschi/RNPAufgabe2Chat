8821df8913531991381ff6d9166abcc55633bb2e
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-11-06 20:06:58 +0100

    Angefangen mit der implementierung des ChatServerWorkThread
    Messages werden schon abgeholt, jetzt müssen noch die entsprechenden
    reaktionen auf die unterschiedlichen messages rein.

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,6 +1,12 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 
 public class ChatServer {
@@ -14,12 +20,33 @@ public class ChatServer {
 	//die Liste aller aktuell angemeldeten Chat-Clients (siehe ChatUser)
 	public List<ChatUser> loggedInClients; //TODO: später private machen
 	
-	
-	
-	//und startet einen neuen Arbeits-Thread für jede Verbindung,
-	class ChatServerWorkThread extends java.lang.Thread {
+	public static void main(String[] args) {
+		ServerSocket welcomeSocket; 
+		Socket connectionSocket; 
 		
+		int counter = 0; //Zählt die erzeugten Bearbeitungs-Threads
+		
+		
+		try { 
+			welcomeSocket = new ServerSocket(PORT_NUMBER);
+		
+			while(true) { //Server sollten immer laufen, vielleicht einen quit-befehl einbauen? 
+				System.out.println("Chat Server: Waiting for connection on TCP Port " + PORT_NUMBER  );
+			
+				connectionSocket = welcomeSocket.accept();
+				
+				(new ChatServerWorkThread(++counter, connectionSocket)).start();
+				
+				
+			} 
+		} catch (IOException e) {
+				e.printStackTrace(System.err);
+		}
+		
+			
 	}
+	
+	
 	
 	/* Wichtig für den befehl NEW aus dem Protokoll*/
 	public void logClientIn(String username, InetAddress hostname) {
@@ -39,6 +66,109 @@ public class ChatServer {
 	
 
 }
+
+//und startet einen neuen Arbeits-Thread für jede Verbindung,
+	class ChatServerWorkThread extends java.lang.Thread {
+		int threadId; //Die id des threads
+		Socket workingSocket; //Der socket der dem Thread zugeteilt wurde.
+		ChatServer assocSrv; //Der server, zu dem der server-thread gehört
+		
+		private BufferedReader inFromClient; 
+		private DataOutputStream outToClient;
+		
+		private boolean serviceRequested = true; //bei BYE vom client wird das hier umgestellt. 
+		
+		//TODO: Entscheiden, welcher der beiden konstruktoren ich brauche.
+		public ChatServerWorkThread(int id, Socket sock) {
+			this.threadId = id;
+			this.workingSocket = sock;
+		}
+		
+		public ChatServerWorkThread(int id, Socket sock, ChatServer associatedServer) {
+			this.threadId = id;
+			this.workingSocket = sock;
+			this.assocSrv = associatedServer;
+		}
+		
+		public void run() {
+			String messageFromClient; //das was der client einem gesendet hat.
+			
+			System.out.println("Chat Server Thread " + threadId 
+					+ " is running on TCP Port " + ChatServer.PORT_NUMBER 
+					+ " until BYE is recieved!");
+			
+			try {
+				/* inputstream vom socket in inputreader und bufferedreader verpacken,
+				 * weil es bequemer ist */ 
+				inFromClient = new BufferedReader(new InputStreamReader(
+						workingSocket.getInputStream()));
+				// Bei outToClient brauchen wir zumindest einen DataOutputStream
+				outToClient = new DataOutputStream(workingSocket.getOutputStream());
+				
+				while (serviceRequested) {
+					messageFromClient = readFromClient();
+					String[] messageParts = messageFromClient.split(" "); // zwischen den teilen der nachricht sind leerzeichen
+					String command = messageParts[0]; // der erste teil der message zeigt an, ob es NEW, INFO oder BYE ist.
+					
+					switch(command) {
+					case "NEW": doNew(messageParts); break;
+					case "INFO": doInfo(messageParts); break; 
+					case "BYE": doBye(messageParts); break;
+					default: String reason = "Unknown Message to Server" ;sendError(reason);
+					}
+				}
+	
+			}  catch (IOException e) {
+				e.printStackTrace(System.err);
+			}
+		}
+		
+		/** Methode, welche das senden von fehlernachrichten an den client 
+		 * verantwortet. Fehler können bei falschen 
+		 * @param reason der grund, warum der server ERROR an den client sendet
+		 */
+		private void sendError(String reason) {
+			
+			
+		}
+
+		/**
+		 * Methode, die ausgeführt werden soll, wenn der Server die nachricht "INFO" bekommen soll.
+		 * @param messageParts die message vom client als zerlegte strings.
+		 */
+		private void doInfo(String[] messageParts) {
+			//TODO: herausfinden, wie ich an die instanzvariable loggedInClients aus ChatServer herankommen soll! 
+			//evtl ChatServer komplett statisch machen, nur die threads sind dann OO
+			
+		}
+
+		private void doBye(String[] messageParts) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doNew(String[] messageParts) {
+			
+			
+		}
+
+		private String readFromClient() throws IOException  {
+			String request = inFromClient.readLine();
+			System.out.println("Chat Server Thread " + threadId + " detected job: " + request);
+			return request;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
 
 
 
