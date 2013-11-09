2cae5be96429d0c9d1e89e9c2224c59b7b171f14
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-11-10 00:51:41 +0100

    Unit-tests für server aufgeräumt
    keine warnings mehr dort

--- test/server_test/ChatServerTest.java
+++ test/server_test/ChatServerTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import server.ChatServer;
@@ -19,15 +18,14 @@ import server.ChatUser;
 
 public class ChatServerTest {
 	
-	private ChatServer c;
 	private ByteArrayOutputStream mySysOut = new ByteArrayOutputStream(); //zum testen von Sysouts
 	
 	
 	
 	@Before 
 	public void setUpServer() {
-		c = new ChatServer();
-		c.loggedInClients = new ArrayList<>();
+		
+		ChatServer.loggedInClients = new ArrayList<>();
 	}
 	@Before 
 	public void setUpMyStreams() {
@@ -36,30 +34,33 @@ public class ChatServerTest {
 
 	@Test
 	public void testLogClientIn() {
-	    c.logClientIn(new ChatUser("Steffen",InetAddress.getLoopbackAddress())); 
+		ChatUser newUser = new ChatUser("Steffen",InetAddress.getLoopbackAddress());
+	    ChatServer.logClientIn(newUser); 
+	    String userName = "Steffen";
+		String hostName = InetAddress.getLoopbackAddress().getCanonicalHostName();
 	    //mySysOut ist jetzt voll mit den println-messages aus logClientIn
 	    List<String> outputs = Arrays.asList(mySysOut.toString().split("\r\n")); //splitten beim newline => jedes sysout benutzt println!
-	    assertTrue("Test Log Client in: Message beim start des einloggens korrekt", outputs.contains(ChatServer.LOGIN_START_MSG));
+	    assertTrue("Test Log Client in: Message beim start des einloggens korrekt", outputs.contains(String.format(ChatServer.LOGIN_START_MSG,userName,hostName)));
 	    //TODO outputs mit expected messages vergleichen.
-	    assertTrue("Test Log Client In: Erfolgreiche Eingeloggt message korrekt",outputs.contains(ChatServer.SUCCESSFUL_LOGIN_MSG));
+	    assertTrue("Test Log Client In: Erfolgreiche Eingeloggt message korrekt",outputs.contains(String.format(ChatServer.SUCCESSFUL_LOGIN_MSG,userName,hostName)));
 	    
 	    assertTrue("Test Log Client In: Client wurde erfolgreich in die liste eingefügt.",
-    			c.loggedInClients.contains(new ChatUser("Steffen",InetAddress.getLoopbackAddress()))
+    			ChatServer.loggedInClients.contains(new ChatUser("Steffen",InetAddress.getLoopbackAddress()))
     		  );
 	    
 	}
 
 	@Test
 	public void testAddClient() {
-		ChatServer c = new ChatServer();
-		c.loggedInClients = new ArrayList<>();
+	
+		ChatServer.loggedInClients = new ArrayList<>();
 		InetAddress address = null;
 		
 	    address = InetAddress.getLoopbackAddress();
 	
-		c.addClient(new ChatUser("Christian",address));
+		ChatServer.addClient(new ChatUser("Christian",address));
 	    assertTrue("Test Add Client: Einfügen eines neuen Clients in die Liste",
-	    			c.loggedInClients.contains(new ChatUser("Christian",address))
+	    			ChatServer.loggedInClients.contains(new ChatUser("Christian",address))
 	    		  );
 	}
 	
