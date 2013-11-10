c0a8458d96c025ad1b887738d6a13f23420042b8
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-11-10 22:28:19 +0100

    ClientServerCommunicator "Merge"
    meine konstante für den regex + steffens neueste bugfixes + korrigierte
    einrückung

--- src/client/ClientServerCommunicator.java
+++ src/client/ClientServerCommunicator.java
@@ -13,109 +13,116 @@ import java.util.List;
 import server.ChatUser;
 
 public class ClientServerCommunicator extends Thread {
- private static final String ONE_OR_MORE_SPACES = " +"; 
+	/* Regulärer ausdruck für String.split, 
+	 * Aus Hübners spezifikation: "Message-Name
+	 * und Parameter werden durch mindestens
+	 * ein Leerzeichen getrennt.", Also
+	 * mindestens ein leerzeichen, danach 
+	 * beliebig viele, das ist " +".
+	 */	
+	private static final String ONE_OR_MORE_SPACES = " +"; 
 
- public static final int SERVER_PORT = 50000;
+	public static final int SERVER_PORT = 50000;
 
- private Socket clientSocket; // TCP-Standard-Socketklasse
+	private Socket clientSocket; // TCP-Standard-Socketklasse
 
- private DataOutputStream outToServer; // Ausgabestream zum Server
- private BufferedReader inFromServer; // Eingabestream vom Server
- 
- private int delayInSeconds = 5;
- 
- String userName;
- String serverHostName;
+	private DataOutputStream outToServer; // Ausgabestream zum Server
+	private BufferedReader inFromServer; // Eingabestream vom Server
 
- public ClientServerCommunicator(String userName, String serverHostName){
-  this.userName = userName;
-  this.serverHostName = serverHostName;
- }
- 
- public void run() {
-  /* Client starten. Ende, wenn quit eingegeben wurde */
-  String response; // vom User übergebener String
+	private int delayInSeconds = 5;
 
-  /* Ab Java 7: try-with-resources mit automat. close benutzen! */
-  try {
-   /* Socket erzeugen --> Verbindungsaufbau mit dem Server */
-   clientSocket = new Socket(serverHostName, SERVER_PORT);
+	String userName;
+	String serverHostName;
 
-   /* Socket-Basisstreams durch spezielle Streams filtern */
-   outToServer = new DataOutputStream(clientSocket.getOutputStream());
-   inFromServer = new BufferedReader(new InputStreamReader(
-     clientSocket.getInputStream()));
+	public ClientServerCommunicator(String userName, String serverHostName){
+		this.userName = userName;
+		this.serverHostName = serverHostName;
+	}
 
-   writeToServer("NEW " + userName);
-   response = readFromServer();
+	public void run() {
+		/* Client starten. Ende, wenn quit eingegeben wurde */
+		String response; // vom User übergebener String
 
-   if (response.equals("OK")) {
-    while (ChatClient.isServiceRequested()) {
-     writeToServer("INFO"); //Get Userlist from Server
-     response = readFromServer();
-     List<ChatUser> users = getUsers(response); //Compute Userlist String
-     ChatClient.users = users; //Replace old UserList
-     Thread.sleep(delayInSeconds*1000); //Wait for 5 seconds
-    }
-    writeToServer("BYE");
-    response = readFromServer();
-    if (!response.equals("BYE")) {
-     throw new Exception(response);
-    }
-   } else {
-    throw new Exception(response);
-   }
-   /* Socket-Streams schließen --> Verbindungsabbau */
-   clientSocket.close();
-  } catch (UnknownHostException e) {
-   e.printStackTrace();
-  } catch (IOException e) {
-   System.err.println("Connection aborted by server!");
-  } catch (Exception e) {
-   e.printStackTrace();
-  }
-  System.out.println("TCP Client stopped!");
- }
+		/* Ab Java 7: try-with-resources mit automat. close benutzen! */
+		try {
+			/* Socket erzeugen --> Verbindungsaufbau mit dem Server */
+			clientSocket = new Socket(serverHostName, SERVER_PORT);
 
- public static List<ChatUser> getUsers(String response)
-   throws UnknownHostException {
-  List<ChatUser> users = new ArrayList<ChatUser>();
-  String[] modifiedResponse;
-  int maxUserIndex; // Index of the last Hostname (Hostname-n) within the
-       // Stringarray "modifiedResponse".
-       // Since the last Hostname is followed by the
-       // corresponding chatname, maxUserIndex will
-       // modifiedResponse.length -1.
+			/* Socket-Basisstreams durch spezielle Streams filtern */
+			outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			inFromServer = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
 
-  modifiedResponse = response.split(ONE_OR_MORE_SPACES); // split Response into the
-            // single Parts
-  maxUserIndex = modifiedResponse.length - 1; // ->see declaration
-  // The Response Array Looks like this:
-  // |List|NumberOfUsers|Hostname1|UserName1|...|HostnameN|UserNameN| for
-  // n = NumberOfUsers and m = size of the Array
-  // Index:  0   1   2   3   ... m-3   m-2   m-1  m
-  // Content: LIST  Number  Host1  User1  ... Hostn-1  Usern-1  Hostn  User
-  // A new User will be found every second Index with an Offset of 2 at the beginning.
-  for (int i = 2; i <= maxUserIndex; i += 2) {
-   InetAddress hostName = InetAddress.getByName(modifiedResponse[i]);
-   String chatName = modifiedResponse[i + 1];
-   ChatUser user = new ChatUser(chatName, hostName);
-   users.add(user);
-  }
-  return users;
- }
+			writeToServer("NEW " + userName);
+			response = readFromServer();
 
- private void writeToServer(String request) throws IOException {
-  /* Sende eine Zeile zum Server */
-  outToServer.writeBytes(request + '\n');
-  System.out.println("TCP Client has sent the message: " + request);
- }
+			if (response.equals("OK")) {
+				while (ChatClient.isServiceRequested()) {
+					writeToServer("INFO"); //Get Userlist from Server
+					response = readFromServer();
+					List<ChatUser> users = getUsers(response); //Compute Userlist String
+					ChatClient.users = users; //Replace old UserList
+					Thread.sleep(delayInSeconds*1000); //Wait for 5 seconds
+				}
+				writeToServer("BYE");
+				response = readFromServer();
+				if (!response.equals("BYE")) {
+					throw new Exception(response);
+				}
+			} else {
+				throw new Exception(response);
+			}
+			/* Socket-Streams schließen --> Verbindungsabbau */
+			clientSocket.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Connection aborted by server!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("TCP Client stopped!");
+	}
 
- private String readFromServer() throws IOException {
-  /* Lies die Antwort (reply) vom Server */
-  String reply = inFromServer.readLine();
-  System.out.println("TCP Client got from Server: " + reply);
-  return reply;
- }
+	public static List<ChatUser> getUsers(String response)
+			throws UnknownHostException {
+		List<ChatUser> users = new ArrayList<ChatUser>();
+		String[] modifiedResponse;
+		int maxUserIndex; // Index of the last Hostname (Hostname-n) within the
+		// Stringarray "modifiedResponse".
+		// Since the last Hostname is followed by the
+		// corresponding chatname, maxUserIndex will
+		// modifiedResponse.length -1.
+
+		modifiedResponse = response.split(ONE_OR_MORE_SPACES); // split Response into the
+		// single Parts
+		maxUserIndex = modifiedResponse.length - 1; // ->see declaration
+		// The Response Array Looks like this:
+		// |List|NumberOfUsers|Hostname1|UserName1|...|HostnameN|UserNameN| for
+		// n = NumberOfUsers and m = size of the Array
+		// Index:  0   1   2   3   ... m-3   m-2   m-1  m
+		// Content: LIST  Number  Host1  User1  ... Hostn-1  Usern-1  Hostn  User
+		// A new User will be found every second Index with an Offset of 2 at the beginning.
+		for (int i = 2; i <= maxUserIndex; i += 2) {
+			InetAddress hostName = InetAddress.getByName(modifiedResponse[i]);
+			String chatName = modifiedResponse[i + 1];
+			ChatUser user = new ChatUser(chatName, hostName);
+			users.add(user);
+		}
+		return users;
+	}
+
+	private void writeToServer(String request) throws IOException {
+		/* Sende eine Zeile zum Server */
+		outToServer.writeBytes(request + '\n');
+		System.out.println("TCP Client has sent the message: " + request);
+	}
+
+	private String readFromServer() throws IOException {
+		/* Lies die Antwort (reply) vom Server */
+		String reply = inFromServer.readLine();
+		System.out.println("TCP Client got from Server: " + reply);
+		return reply;
+	}
 
 }
